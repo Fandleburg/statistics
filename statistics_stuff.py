6d8cae9dee6792ffd3a4d27bd7b71d78d3a067b1
Chris Crosbie <ehsteve84@gmail.com>
2025-11-21 16:29:51 +1100

    Added variable type.

--- statistics_stuff.py
+++ statistics_stuff.py
@@ -4,7 +4,7 @@ import logging
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
-def get_standard_deviation(sample):
+def get_standard_deviation(sample: list):
     import math
 
     mean = sum(sample) / len(sample)                    # Average value in sample.
@@ -13,7 +13,7 @@ def get_standard_deviation(sample):
     variance = sum(squared_deviations) / len(sample)    # Get the average of the squared deviations.
     return math.sqrt(variance)                          # Square root to get the deviation in the original "un-squared" scale.
 
-def one_sample_t_test(sample, hypothosis_mean):
+def one_sample_t_test(sample: list, hypothosis_mean):
     import math
 
     mean = sum(sample) / len(sample)
@@ -23,7 +23,7 @@ def one_sample_t_test(sample, hypothosis_mean):
 
     return (mean - hypothosis_mean) / (standard_deviation / math.sqrt(sample_size))
 
-def p_value(sample, hypothosis_mean):
+def p_value(sample: list, hypothosis_mean):
     from scipy import stats
 
     t_value = one_sample_t_test(sample, hypothosis_mean)
@@ -33,4 +33,5 @@ def p_value(sample, hypothosis_mean):
 
     logger.info(f"The t-value is: {t_value}")
     logger.info(f"In a t-distribution table, look at the line where the degrees of freedom (df) is {degrees_of_freedom} and identify which columns (p-levels) the t-value {t_value:.3f} is between.")
+    logger.info("Example t-distribution table: https://www.stat.purdue.edu/~lfindsen/stat503/t-Dist.pdf")
     logger.info(f"Being a one-sample t-test, getting the average of these p-levels will give you the p-value. (If it was a two-sample test, multiply this by 2 to get the p-value)")
